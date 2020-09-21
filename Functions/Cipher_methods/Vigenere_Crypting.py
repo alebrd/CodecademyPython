0740a8114357335c2a247c2012396351d9681158
alebrd <71253112+alebrd@users.noreply.github.com>
2020-09-21 18:23:54 +0200

    Cipher updates

---
+++ Functions/Cipher_methods/Vigenere_Crypting.py
@@ -0,0 +1,25 @@
+def vigenere_coder(message, keyword):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    punctuation = ".,?'! "
+    letter_pointer = 0
+    keyword_final = ''
+    for i in range(0,len(message)):
+        if message[i] in punctuation:
+            keyword_final += message[i]
+        else:
+            keyword_final += keyword[letter_pointer]
+            letter_pointer = (letter_pointer+1)%len(keyword)
+    translated_message = ''
+    for i in range(0, len(message)):
+        if message[i] not in punctuation:
+            ln = alphabet.find(message[i]) + alphabet.find(keyword_final[i])
+            translated_message += alphabet[ln % 26]
+        else:
+            translated_message += message[i]
+    return translated_message
+
+
+message_for_v = "thanks for teaching me all these cool ciphers! you really are the best!"
+keyword = "friends"
+
+print(vigenere_coder(message_for_v, keyword))
