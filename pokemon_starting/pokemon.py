5577ee56ca8a37da32c04437fc24794640ae7115
alebrd <71253112+alebrd@users.noreply.github.com>
2020-10-06 15:39:09 +0200

    Class: 'Basta pokemon.py  Project'.

--- pokemon_starting/pokemon.py
+++ pokemon_starting/pokemon.py
@@ -1,4 +1,3 @@
-import inspect
 class Pokemon:
     def __init__(self, name, level, type_, health):
         self.name = name
@@ -9,64 +8,64 @@ class Pokemon:
         self.is_knocked_out = False
 
     def lose_health(self, points):
-        if type(points) is int:
+        if type(points) is int or float:
             self.health -= points
         return 'Your {} Pokemon has now {} health.'.format(self.name, self.health)
 
     def gain_health(self, points):
         if type(points) is int:
             self.health += points
-            if self.health > self.max_health:
-                self.health = self.max_health
+            if self.health >= self.max_health:
+                new_health = self.max_health - 20
+                self.health = new_health
         return 'Your {} Pokemon has now {} health.'.format(self.name, self.health)
 
     def knockedout(self):
         if self.health == 0:
             self.is_knocked_out = True
         return 'Your {} has 0 health points so is gone.'.format(self.name)
+
     def get_name(self):
         return self.name
 
     def attack(self, another_pokemon):
         points = 25
-        if type(another_pokemon) is Pokemon:
-            if self.type_ == 'Fire':
-                if str(another_pokemon.type_) == 'Grass':
-                    points *= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Water':
-                    points /= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Fire':
-                    points /= 2
-                another_pokemon.lose_health(points)
-            elif self.type_ == 'Grass':
-                if another_pokemon.type_ == 'Fire':
-                    points /= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Water':
-                    points *= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Grass':
-                    points /= 2
-                another_pokemon.lose_health(points)
-            elif self.type_ == 'Water':
-                if another_pokemon.type_ == 'Grass':
-                    points /= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Fire':
-                    points *= 2
-                #    another_pokemon.lose_health(points)
-                elif another_pokemon.type_ == 'Water':
-                    points /= 2
-                another_pokemon.lose_health(points)
-        print('-Your {} is type: {}.\n'
-              '-Enemy\'s {} is type: {}.\n'
-              '-The Enemy\'s Pokemon has lost {} points.\n'
-              '-Your Pokemon current Health is {}.\n'
-              '-Enemy\'s Pokemon Health is {}'.format(self.name, self.type_, another_pokemon.name,
-                                                      another_pokemon.type_, points,
-                                                      self.health, another_pokemon.health))
+        if self.is_knocked_out != False:
+            if type(another_pokemon) is Pokemon:
+                if self.type_ == 'Fire':
+                    if str(another_pokemon.type_) == 'Grass':
+                        points *= 2
+                    elif another_pokemon.type_ == 'Water':
+                        points /= 2
+                    elif another_pokemon.type_ == 'Fire':
+                        points /= 2
+                    another_pokemon.lose_health(points)
+                elif self.type_ == 'Grass':
+                    if another_pokemon.type_ == 'Fire':
+                        points /= 2
+                    elif another_pokemon.type_ == 'Water':
+                        points *= 2
+                    elif another_pokemon.type_ == 'Grass':
+                        points /= 2
+                    another_pokemon.lose_health(points)
+                elif self.type_ == 'Water':
+                    if another_pokemon.type_ == 'Grass':
+                        points /= 2
+                    elif another_pokemon.type_ == 'Fire':
+                        points *= 2
+                    elif another_pokemon.type_ == 'Water':
+                        points /= 2
+                    another_pokemon.lose_health(points)
+                return '-Your {} is type: {}.\n' \
+                       '-Enemy\'s {} is type: {}.\n' \
+                       '-The Enemy\'s Pokemon has lost {} points.\n' \
+                       '-Your Pokemon current Health is {}.\n' \
+                       '-Enemy\'s Pokemon Health is {}'.format(self.name, self.type_, another_pokemon.name,
+                                                               another_pokemon.type_, points,
+                                                               self.health, another_pokemon.health)
+        else:
+            return 'Your Pokemon is Knocked out'
+
 
 
 class Trainer:
@@ -75,17 +74,18 @@ class Trainer:
         self.pokemons = pokemons  # Must be a list of Pokemon Class.
         self.current_pokemon = current_pokemon  # Pokemon selected
         self.potions = potions  # potion that heal the current pokemon selected
-        self.current_pokemon_name = self.current_pokemon.name
+
+    #    self.current_pokemon_name = self.current_pokemon.name
 
     def __repr__(self):
         return str(self.current_pokemon)
 
     def attack_other_trainer(self, other_trainer):
+        global attack
         if type(other_trainer) is Trainer:
-            their_pokemon = other_trainer.current_pokemon
-            current = self.current_pokemon
-            if type(current) is Pokemon:
-                current.attack(their_pokemon)
+            #    their_pokemon = other_trainer.current_pokemon
+            attack = self.current_pokemon.attack(other_trainer.current_pokemon)
+        return attack
 
     def heal_current_pokemon(self):
         if self.potions > 0:
@@ -93,22 +93,23 @@ class Trainer:
             self.potions -= 1
             if type(current) is Pokemon:
                 current.gain_health(100)
-        return f'Your current Pokemon \'{self.current_pokemon}\' has healed'
+        return f'Your current Pokemon \'{self.current_pokemon.name}\' has healed.'
 
     def switch_current(self, name):
         if type(name) is str:
             for switch in self.pokemons:
+                if switch.is_knocked_out != False:
+                    break
                 switch_name = switch.get_name()
-                if name == switch_name:
+                if str(name) in str(switch_name):
                     self.current_pokemon = switch
-                # for i in switch:
-                #     if name is in self.pokemons.name:
-                #         self.current_pokemon = switch
-        return self.current_pokemon.name
+            return f'Your current no is {self.current_pokemon.name}.\n' \
+                   f'If your Pokemon does not change is probably dead.'
+        else:
+            return 'Please insert a string'
 
 
-
-Gabri = Pokemon('Gabri', 10, 'Fire', 100)
+Gabri = Pokemon('Gabri', 10, 'Fire', 0)
 Niki = Pokemon('Niki', 10, 'Grass', 100)
 Thomas = Pokemon('Thomas', 10, 'Grass', 100)
 Gianfro = Pokemon('Gianfro', 10, 'Water', 100)
@@ -118,7 +119,4 @@ Gianfro = Pokemon('Gianfro', 10, 'Water', 100)
 Alessandro = Trainer('Alessandro', [Gabri], Gabri, 5)
 Pezzone = Trainer('Pezzone', [Niki, Thomas, Gianfro], Niki, 5)
 
-Pezzone.attack_other_trainer(Alessandro)
 
-#print(Pezzone.current_pokemon_name)
-Pezzone.switch_current('Thomas')
